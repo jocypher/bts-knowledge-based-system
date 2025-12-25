c302f2400bcd8422239fc53576116935566cba49
willyjo1 <arthurwilchield@gmail.com>
2025-12-25 17:48:26 +0000

    update to db system

--- dist/db/db.js
+++ dist/db/db.js
@@ -2,6 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Database = require("better-sqlite3");
 const path = require("path");
+const fs = require("fs");
+const dbDir = path.join(process.cwd(), "data");
+// Create folder if it doesn't exist
+if (!fs.existsSync(dbDir)) {
+    fs.mkdirSync(dbDir, { recursive: true });
+}
 const dbPath = process.env.DB_PATH || path.join(process.cwd(), "data", "bts_database.db");
 const db = Database(dbPath, {
     verbose: console.log
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,13 @@
 const Database = require("better-sqlite3")
 const path = require("path")
+const fs = require("fs")
 
 
+const dbDir = path.join(process.cwd(), "data");
+// Create folder if it doesn't exist
+if (!fs.existsSync(dbDir)) {
+  fs.mkdirSync(dbDir, { recursive: true });
+}
 const dbPath = process.env.DB_PATH || path.join(process.cwd(), "data","bts_database.db")
 const db = Database(dbPath,{
     verbose: console.log
