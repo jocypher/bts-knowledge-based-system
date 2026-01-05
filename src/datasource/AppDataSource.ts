116a7e72d45b2b24e755acaf39e9e3743cca8a1d
willyjo1 <arthurwilchield@gmail.com>
2026-01-05 10:29:45 +0000

    update: fix db issue

--- src/datasource/AppDataSource.ts
+++ src/datasource/AppDataSource.ts
@@ -9,7 +9,7 @@ const isProduction = process.env.NODE_ENV === "production";
 const AppDataSource = new DataSource({
     type: "postgres",
     url: process.env.DATABASE_URL,
-    synchronize: false,
+    synchronize: true,
     
     extra: {
         max: 5,
